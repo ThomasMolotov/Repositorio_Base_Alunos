0195c33a8ec65eadbb2650fcea06d52eebdd0ec5
Mestre dos Trotes <caiquevitor70@gmail.com>
2025-09-10 10:40:51 -0300

    Add files via upload

---
+++ Modulo 1/Extra/funcoes.py
@@ -0,0 +1,23 @@
+def escreva():
+    print("eae fí")
+
+escreva()
+
+
+def escreva(msg):
+    print(msg)
+
+escreva("eae fíote")
+
+
+def soma(n1,n2):
+    print(n1 + n2)
+
+soma(1,2)
+soma(3,4)
+
+def somar(n1,n2):
+    return n1 + n2
+
+print(somar(6,2))
+somar(3,3)
