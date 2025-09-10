1b81b4271046414df42796575c0d2ecb92204a5f
Mestre dos Trotes <caiquevitor70@gmail.com>
2025-09-10 10:23:42 -0300

    Add files via upload

---
+++ Modulo 1/Codigos Basicos/carro.py
@@ -0,0 +1,5 @@
+carro = input("Qual é o seu carro?: ")
+valor = float(input("Qual o valor do carro?: "))
+consumo = float(input("Quanto ele consome?: "))
+
+print(f"O seu carro é: {carro} / O valor do carro é de: R${valor} / O consumo dele é de: {consumo} Km/l")
