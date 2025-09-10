72b3ba2e0af1be00fd8431e5411c0fa72b2c50da
Mestre dos Trotes <caiquevitor70@gmail.com>
2025-09-10 10:38:47 -0300

    Add files via upload

---
+++ Modulo 1/Listas/exer7.py
@@ -0,0 +1,13 @@
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite um número: "))
+n3 = int(input("Digite um número: "))
+n4 = int(input("Digite um número: "))
+n5 = int(input("Digite um número: "))
+
+soma = n1 + n2 + n3 + n4 + n5
+mult = n1 * n2 * n3 * n4 * n5
+nums = [n1, n2, n3, n4, n5]
+
+print(soma)
+print(mult)
+print(nums)
